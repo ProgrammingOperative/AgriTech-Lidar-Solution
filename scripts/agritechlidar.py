6ab0bfd631fbd287873bb9529e3334e287679439
waCira <wachura11t@gmail.com>
2022-06-23 01:42:39 +0300

    Create the AgriTechLidar class

---
+++ scripts/agritechlidar.py
@@ -0,0 +1,12 @@
+from shapely.geometry import Polygon
+from fetch_lidar import FetchLidar
+
+
+
+
+class AgriTechLidar:
+    def __init__(self) -> None:
+        pass
+
+    def fetch_lidar(self, polygon: Polygon):
+        return self.fetch_lidar(polygon)
---
+++ scripts/fetch_lidar.py
@@ -0,0 +1,14 @@
+import pdal
+import json
+import pandas as pd
+import geopandas as gpd
+from shapely.geometry import Polygon
+
+
+class FetchLidar:
+    def __init__(self) -> None:
+        pass
+
+
+
+
