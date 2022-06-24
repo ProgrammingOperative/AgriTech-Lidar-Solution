e523e417168684e0e1b151b9fc6793f676449e0b
waCira <wachura11t@gmail.com>
2022-06-25 02:18:14 +0300

    Configure paths to asset

--- scripts/fetch_lidar.py
+++ scripts/fetch_lidar.py
@@ -6,25 +6,23 @@ from shapely.geometry import Polygon
 import json
 from config import Config
 
-
 class FetchLidar:
     def __init__(self) -> None:
         pass
 
 
 
-    def get_pipeline(self, bounds: str, polygon_str: str, region: str, filename: str):
+    def make_pipeline(self, bounds: str, polygon_str: str, region: str, filename: str):
         
         with open('../assets/usgs_3dep_pipeline', 'r') as json_file:
             json_object = json.load(json_file)
 
         pipe = json_object
-        pipe['pipeline'][0]['filename'] = path
+        pipe['pipeline'][0]['filename'] = Config.USGS_3DEP_PUBLIC_DATA_PATH / f"{region}/etp.json"
         pipe['pipeline'][0]['bounds'] = bounds
         pipe['pipeline'][1]['polygon'] = polygon_str
         pipe['pipeline'][6]['out_srs'] = f'EPSG:{self.output_epsg}'
-        pipe['pipeline'][7]['filename'] = path
-        pipe['pipeline'][8]['filename'] = path
+        pipe['pipeline'][7]['filename'] = Config.ASSETS_PATH + f'{filename}.tif' 
         return pdal.Pipeline(json.dumps(pipe))
 
 
