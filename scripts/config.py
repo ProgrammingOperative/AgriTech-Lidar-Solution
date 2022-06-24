fd432c199b6083399909d0f4f9e777cf2ebbd60b
waCira <wachura11t@gmail.com>
2022-06-24 19:37:04 +0300

    Experiment with pipeline

--- scripts/config.py
+++ scripts/config.py
@@ -7,3 +7,6 @@ class Config:
   DATA_PATH = ROOT_PATH / "data/"
   ASSETS_PATH = ROOT_PATH / "assets/"
   USGS_3DEP_PUBLIC_DATA_PATH = "https://s3-us-west-2.amazonaws.com/usgs-lidar-public/"
+  LAZ_PATH = DATA_PATH / "laz"
+  TIF_PATH = DATA_PATH / "tif"
+  SHP_PATH = DATA_PATH / "shp"
