38a70b965cfc4749367efaa2b2bf0f86910d3b73
waCira <wachura11t@gmail.com>
2022-06-25 23:31:46 +0300

    Create geopandas heper class

---
+++ scripts/gpd_helper.py
@@ -0,0 +1,10 @@
+import numpy as np
+import geopandas as gpd
+from bounds import Bounds
+from shapely.geometry import Point, Polygon
+
+
+
+class GpdHelper:
+    def __init__(self) -> None:
+        pass
