46588c55ea7ea775c458072f93383608332589d1
waCira <wachura11t@gmail.com>
2022-06-26 13:06:17 +0300

    initialize visualization class

---
+++ scripts/visualization.py
@@ -0,0 +1,12 @@
+import os
+from glob import glob
+import matplotlib.pyplot as plt
+import rasterio as rio
+from rasterio.plot import plotting_extent
+import geopandas as gpd
+
+
+
+class Visualize():
+    def __init__(self) -> None:
+        pass
